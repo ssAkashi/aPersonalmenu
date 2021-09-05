e7f7a2853dcb7d0b8d641aa82a77bfa6394738b0
ssAkashi <akashiw3bdev@outlook.fr>
2021-09-05 23:53:05 +0200

    add personalmenu

---
+++ french/aknx_personalmenu/shared/config_webhooks.lua
@@ -0,0 +1,10 @@
+--[[
+    exemple :
+    Config.Webhooks.LogXXX = "YOUR WEBHOOK LINK"
+]]
+
+Config.Webhooks.LogTransferItem = ""
+Config.Webhooks.LogTransferWeapon = ""
+Config.Webhooks.LogTransferMoney = ""
+Config.Webhooks.LogAdminMenu = ""
+Config.Webhooks.ModderDetect = ""
